08e729f24d41289a90d23747a3af0bf2d30392b8
badasahog <yairkatz10@gmail.com>
2020-05-16 21:19:56 +0300

    fa

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import React from "react";
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  
+  import React from "react";
 import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 
@@ -68,14 +82,3 @@ const ExampleDynamicColumns = () => {
 };
 export default ExampleDynamicColumns;
 render(<ExampleDynamicColumns />, document.getElementById("app"));
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
