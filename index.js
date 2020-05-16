7a4904e153c401dcb8c30838403155d3f37bea7f
badasahog <yairkatz10@gmail.com>
2020-05-16 21:13:13 +0300

    fixed Node.js alignment

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
+import React from "react";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  }
+];
+
+
+function columns(containerWidth) {
+  let columns = 1;
+  if (containerWidth >= 500) columns = 2;
+  if (containerWidth >= 900) columns = 3;
+  if (containerWidth >= 1500) columns = 4;
+  return columns;
+}
+
+const ExampleDynamicColumns = () => {
+  return (
+    <div>
+      <Gallery photos={photos} direction="column" columns={columns} />
+    </div>
+  );
+};
+one;
+export default ExampleDynamicColumns;
+render(<ExampleDynamicColumns />, document.getElementById("app"));
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
